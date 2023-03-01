04c3230d57ca089c64e9e01bee0c2ba29440ba9f
Sabit Huraira <sabitzhabit@gmail.com>
2023-03-01 12:43:42 +0700

    serch - add header

--- src/components/search/TopContent.svelte
+++ src/components/search/TopContent.svelte
@@ -1,94 +1,28 @@
-<script>
-	import { infoWilayah, parentWilayah, childWilayah } from '../../stores/wilayahStores';
-
-	let info_wilayah = null;
-
-	infoWilayah.subscribe((value) => {
-		info_wilayah = value;
-	});
-</script>
-
 <section class="wrapper image-wrapper bg-image bg-overlay bg-overlay-900 bg-overlay-900 "
-	data-image-src="/images/img/desa/desa_1.jpg">
-	<div class="container pt-6 pt-md-15 pb-8">
-		<div class="row gx-lg-8 gy-8 mt-5 mt-md-12 mt-lg-0 mb-8 align-items-center">
+	data-image-src="/images/landing/descan_11.JPG">
+	<div class="container pt-2 pt-md-15 pb-2">
+		<div class="row gx-lg-8 gy-8 mt-1 mt-md-12 mt-lg-0 mb-3 align-items-center">
 			<div class="col-lg-6 order-lg-2">
 				<div class="row gx-md-5 gy-5" data-cues="fadeIn" data-group="images">
-				<div class="col-md-6 text-center ">
-					<figure class="rounded mx-4"><img src="/images/prov-logo/prov_logo.png"
-						srcset="/images/prov-logo/prov_logo.png 2x" alt=""
-						style="max-height: 300px; max-width: 200px; height:100% ; width: 100%;"></figure>
-				</div>
-				<div class="col-md-6 text-center ">
-					<figure class="rounded mx-4"><img src="/images/kabs-logo/logo1601.png"
-						srcset="/images/kabs-logo/logo1601.png 2x" alt=""
-						style="max-height: 300px; max-width: 200px;  height:100% ; width: 100%;"></figure>
-				</div>
+					<div class="col-md-3 text-center">
+					</div>
+					<div class="col-md-9  pb-4 text-center">
+						<figure class="rounded mx-4"><img src="/images/prov-logo/prov_logo.png"
+							srcset="/images/prov-logo/prov_logo.png 2x" alt=""
+							style="max-height: 300px; max-width: 200px; height:100% ; width: 100%;"></figure>
+					</div>
 				</div>
 			</div>
-			<div class="col-lg-6 text-white">
-				<h2 class="display-4 mb-3 text-white">{ info_wilayah.nama }</h2>
-				<p class="lead fs-lg mb-8 pe-xxl-2">Desa Penghasil Karet Terbanyak di Kabupaten Ogan Komering
-				Ulu</p>
-				<div class="row gx-xl-10 gy-6" data-cues="slideInUp" data-group="services">
-				<div class="col-md-6 col-lg-12 col-xl-6">
-					<button class="d-flex flex-row btn text-start p-0" id="btn_luas_wilayah">
-					<div>
-						<div class="icon btn btn-circle btn-lg btn-soft-primary disabled me-5">
-						<i class="uil uil-map"></i>
-						</div>
-					</div>
-					<div>
-						<h4 class="mb-1 text-white">Luas Wilayah</h4>
-						<h2 class="mb-0  text-white">12 Km<sup>2</sup></h2>
-					</div>
-					</button>
-				</div>
 
-				<!--/column -->
-				<div class="col-md-6 col-lg-12 col-xl-6">
-					<button class="d-flex flex-row btn text-start p-0" id="btn_penduduk">
-					<div>
-						<div class="icon btn btn-circle btn-lg btn-soft-primary disabled me-5">
-						<i class="uil uil-users-alt"></i>
-						</div>
-					</div>
-					<div>
-						<h4 class="mb-1 text-white">Penduduk</h4>
-						<h2 class="mb-0  text-white">7.385 jiwa</h2>
-					</div>
-
-					</button>
-				</div>
-				<!--/column -->
-				<div class="col-md-6 col-lg-12 col-xl-6">
-					<button class="d-flex flex-row btn text-start p-0" id="btn_faskes">
-					<div>
-						<div class="icon btn btn-circle btn-lg btn-soft-primary disabled me-5">
-						<i class="uil uil-hospital"></i>
-						</div>
-					</div>
-					<div>
-						<h4 class="mb-1 text-white">Fasilitas Kesehatan</h4>
-						<h2 class="mb-0  text-white">16</h2>
-					</div>
-
-					</button>
-				</div>
-				<!--/column -->
-				<div class="col-md-6 col-lg-12 col-xl-6">
-					<button class="d-flex flex-row btn text-start p-0" id="btn_tempat_ibadah">
-					<div>
-						<div class="icon btn btn-circle btn-lg btn-soft-primary disabled me-5">
-						<i class="uil uil-moon"></i>
-						</div>
-					</div>
-					<div>
-						<h4 class="mb-1 text-white">Tempat Ibadah</h4>
-						<h2 class="mb-0  text-white">38</h2>
-					</div>
-					</button>
-				</div>
+            <div class="col-lg-6 text-white">
+				<div data-cues="slideInDown"
+				  data-group="page-title" data-delay="600">
+				  <h1 class="text-white mb-4">
+					Provinsi Sumatera Selatan<br />
+					<span class="typer text-sky" data-delay="100" data-words="Sumatera Selatan"></span>
+					<span class="cursor text-sky" data-owner="typer"></span>
+				  </h1>
+				  <p class="lead fs-24 lh-sm text-white mb-7">Propinsi Sumatera Selatan sejak berabad yang lalu dikenal juga dengan sebutan Bumi Sriwijaya, pada abad ke-7 hingga abad ke-12 Masehi wilayah ini merupakan pusat kerajaan Sriwijaya yang juga terkenal dengan kerajaan maritim terbesar dan terkuat di Nusantara</p>
 				</div>
 			</div>
 		</div>
