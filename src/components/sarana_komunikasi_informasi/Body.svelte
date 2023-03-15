6353215137878ac4883ab54dbd25b6116b67e610
mkharis <081332210xxx@gmail.com>
2023-03-15 09:22:35 +0700

    label option in sarana

--- src/components/sarana_komunikasi_informasi/Body.svelte
+++ src/components/sarana_komunikasi_informasi/Body.svelte
@@ -1,34 +1,7 @@
 <script>
   // @ts-nocheck
   import { informasiKomunikasiInformasi } from "../../stores/infraKomunikasiInformasiStores";
-
-  let definisi_variabel = [
-    {
-      nama_variabel:
-        "Keberadaan internet untuk warnet, game online, dan fasilitas lainnya",
-      nilai: { 1: "Ada", 2: "Tidak Ada" },
-    },
-    {
-      nama_variabel:
-        "Sinyal internet telepon seluler handphone di sebagian besar",
-      nilai: {
-        1: "4G/LTE",
-        2: "3G/H/H+/EVDO",
-        3: "2,5G/E/GPRS",
-        4: "Tidak ada sinyal internet",
-      },
-    },
-    {
-      nama_variabel:
-        "Keberadaan internet untuk warnet, game online, dan fasilitas lainnya",
-      nilai: {
-        1: "Berfungsi",
-        2: "Jarang berfungsi",
-        3: "Tidak berfungsi",
-        4: "Tidak ada",
-      },
-    },
-  ];
+  import { labelOption } from "../../helper/labelOption";
 
   const judul = "Sarana Komunikasi dan Informasi";
 
@@ -36,14 +9,9 @@
 
   informasiKomunikasiInformasi.subscribe((value) => {
     if (value) {
-      rekapContent = definisi_variabel.map((v) => ({
+      rekapContent = value.map((v) => ({
         label: v.nama_variabel,
-        nilai:
-          v.nilai[
-            value.find((n) => n.nama_variabel == v.nama_variabel)
-              ? value.find((n) => n.nama_variabel == v.nama_variabel).nilai
-              : ""
-          ],
+        nilai: labelOption(v.deskripsi_variabel, v.nilai),
       }));
     }
   });
