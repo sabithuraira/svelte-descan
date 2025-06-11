021f604331ba793a14796c46aeac9430db00d172
mkharis <081332210xxx@gmail.com>
2025-06-11 09:18:47 +0700

    chnge sddi narration case and search narration

--- src/components/search/TopContent.svelte
+++ src/components/search/TopContent.svelte
@@ -2,10 +2,8 @@
 	data-image-src="/images/landing/songket.png">
 	<div class="container pt-2 pt-md-15 pb-2">
 		<div class="row gx-lg-8 gy-8 mt-1 mt-md-12 mt-lg-0 mb-3 align-items-center">
-			<div class="col-lg-6 order-lg-2">
+			<div class="col-lg-4 order-lg-2">
 				<div class="row gx-md-5 gy-5" data-cues="fadeIn" data-group="images">
-					<div class="col-md-3 text-center">
-					</div>
 					<div class="col-md-9  pb-4 text-center">
 						<figure class="rounded mx-4"><img src="/images/prov-logo/prov_logo.png"
 							srcset="/images/prov-logo/prov_logo.png 2x" alt=""
@@ -14,7 +12,7 @@
 				</div>
 			</div>
 
-            <div class="col-lg-6 text-white">
+      <div class="col-lg-8 text-white">
 				<div data-cues="slideInDown"
 				  data-group="page-title" data-delay="600">
 				  <h1 class="text-white mb-4">
@@ -22,7 +20,8 @@
 					<span class="typer text-sky" data-delay="100" data-words="Sumatera Selatan"></span>
 					<span class="cursor text-sky" data-owner="typer"></span>
 				  </h1>
-				  <p class="lead fs-24 lh-sm text-white mb-7">Propinsi Sumatera Selatan sejak berabad yang lalu dikenal juga dengan sebutan Bumi Sriwijaya, pada abad ke-7 hingga abad ke-12 Masehi wilayah ini merupakan pusat kerajaan Sriwijaya yang juga terkenal dengan kerajaan maritim terbesar dan terkuat di Nusantara</p>
+				  <p class="lead fs-18 lh-sm text-white mb-7">Provinsi Sumatera Selatan, yang dikenal sebagai Bumi Sriwijaya, pernah menjadi pusat kejayaan Kerajaan Sriwijaya pada abad ke-7 hingga ke-12 Masehi. Kerajaan maritim terbesar di Nusantara dan Asia Tenggara ini memainkan peran penting dalam jalur pelayaran dan perdagangan internasional, serta menjadi titik temu peradaban dunia.<br />
+Warisan sejarah tersebut membentuk Sumatera Selatan sebagai daerah yang kaya budaya, berdaya saing ekonomi, dan strategis dalam pembangunan nasional. Melalui aplikasi ini, data dan informasi seluruh desa di Sumatera Selatan disajikan untuk mendukung perencanaan, pelayanan publik, serta pemberdayaan masyarakat yang berbasis data.</p>
 				</div>
 			</div>
 		</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -231,8 +231,8 @@
     <div class="container">
       <div class="card shadow-none bg-pale-yellow card-border-bottom border-soft-yellow">
         <div class="card-body">
-          <p class="text-justify">DESA DAN POTENSINYA HARUS TERSAJI DENGAN BAIK SEBAGAI INFORMASI KEWILAYAHAN, GUNA MENDUKUNG PEMBANGUNAN PADA TINGKAT PEMERINTAHAN TERKECIL, PEMERINTAH PROVINSI SUMATERA SELATAN DAN BADAN PUSAT STATISTIK PROVINSI SUMATERA SELATAN HADIR BERSAMA BERKOLABORASI MEWUJUDKAN ”SATU DATA DESA INDONESIA” DALAM WUJUD “DESA BUMI SRIWIJAYA”.</p>
-          <p class="text-justify">“DESA BUMI SRIWIJAYA” MENYAJIKAN INFORMASI LENGKAP TENTANG POTENSI EKONOMI DAN KONDISI INFRASTRUKTUR DESA DAN KELURAHAN DI SELURUH WILAYAH SUMATERA SELATAN SEBAGAI UPAYA MENINGKATKAN LITERASI MASYARAKAT DESA UNTUK MENGERTI, MEMAHAMI DAN MEMANFAATKAN DATA UNTUK MEMBANGUN DESANYA. DENGAN “DESA BUMI SRIWIJAYA” MEWUJUDKAN KEBIJAKAN BERBASIS DATA UNTUK DESA YANG LEBIH MAJU MENUJU SUMSEL TERUS MAJU UNTUK SEMUA.</p>
+          <p class="text-justify">Desa dan potensinya harus tersaji dengan baik sebagai informasi kewilayahan, guna mendukung pembangunan pada tingkat pemerintahan terkecil. Pemerintah Provinsi Sumatera Selatan dan Badan Pusat Statistik Provinsi Sumatera Selatan hadir bersama berkolaborasi mewujudkan ”Satu Data Desa Indonesia” dalam wujud “Desa Bumi Sriwijaya”.</p>
+          <p class="text-justify">“Desa Bumi Sriwijaya” menyajikan informasi lengkap tentang potensi ekonomi dan kondisi infrastruktur desa dan kelurahan di seluruh wilayah Sumatera Selatan sebagai upaya meningkatkan literasi masyarakat desa untuk mengerti, memahami, dan memanfaatkan data untuk membangun desanya. Dengan “Desa Bumi Sriwijaya”, mewujudkan kebijakan berbasis data untuk desa yang lebih maju menuju Sumsel terus maju untuk semua.</p>
         </div>
       </div>
     </div>
