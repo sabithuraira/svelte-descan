d83df3d74552b7ec10e4e65089680572b38099ba
Sabit Huraira <sabitzhabit@gmail.com>
2026-02-12 17:03:27 +0700

    idm - change top content

--- src/components/idm/TopContent.svelte
+++ src/components/idm/TopContent.svelte
@@ -1,33 +1,28 @@
 <section class="wrapper image-wrapper bg-image"
-	data-image-src="/images/landing/songket.png">
+	data-image-src="/images/landing/songket.png"
+	style="background-image: url('/images/landing/songket.png'); background-size: cover; background-position: center; min-height: 280px;">
 	<div class="container pt-2 pt-md-15 pb-2">
-		<div class="row mb-3">
-			<div class="col-12">
-				<a href="/" class="btn btn-sm text-white" style="background-color: rgba(148, 49, 38, 0.8); border: none;">
-					<i class="uil uil-home me-1"></i> Beranda
-				</a>
-			</div>
-		</div>
 		<div class="row gx-lg-8 gy-8 mt-1 mt-md-12 mt-lg-0 mb-3 align-items-center">
 			<div class="col-lg-4 order-lg-2">
 				<div class="row gx-md-5 gy-5" data-cues="fadeIn" data-group="images">
-					<div class="col-md-9  pb-4 text-center">
+					<div class="col-md-9 pb-4 text-center">
 						<figure class="rounded mx-4"><img src="/images/prov-logo/prov_logo.png"
-							srcset="/images/prov-logo/prov_logo.png 2x" alt=""
-							style="max-height: 300px; max-width: 200px; height:100% ; width: 100%;"></figure>
+							srcset="/images/prov-logo/prov_logo.png 2x" alt="Logo Provinsi Sumatera Selatan"
+							style="max-height: 300px; max-width: 200px; height:100%; width: 100%;"></figure>
 					</div>
 				</div>
 			</div>
 
-      <div class="col-lg-8 text-white">
-				<div data-cues="slideInDown"
-				  data-group="page-title" data-delay="600">
-				  <h1 class="text-white mb-4">
-					Indeks Desa Membangun (IDM)<br />
-					Provinsi <br />
-					<span class="typer text-sky" data-delay="100" data-words="Sumatera Selatan"></span>
-					<span class="cursor text-sky" data-owner="typer"></span>
-				  </h1>
+			<div class="col-lg-8 text-white">
+				<div data-cues="slideInDown" data-group="page-title" data-delay="600">
+					<h1 class="text-white mb-4">
+						Indeks Desa Membangun (IDM)<br />
+						Provinsi <br />
+						<span class="text-sky">Sumatera Selatan</span>
+					</h1>
+					<p class="lead fs-18 lh-sm text-white mb-7">
+						Indeks Desa Membangun (IDM) adalah indikator komposit untuk mengukur tingkat kemandirian dan ketahanan desa. Data IDM Provinsi Sumatera Selatan disajikan dalam bentuk tabel, grafik, dan peta untuk mendukung perencanaan pembangunan dan pemberdayaan masyarakat desa di Bumi Sriwijaya.
+					</p>
 				</div>
 			</div>
 		</div>
